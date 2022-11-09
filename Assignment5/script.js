2feb329e098a04b1c8be70a3dd7d2bad3f90a5c2
Your Name <you@example.com>
2022-11-09 16:46:58 -0500

    commit

--- Assignment5/script.js
+++ Assignment5/script.js
@@ -6,10 +6,23 @@ var ctx = canvas.getContext("2d");
 
 //square
 ctx.fillStyle = "yellow";
-ctx.strokeStyle = "5px, solid, black, wide";
+ctx.strokeStyle = "solid, black, wide";
+ctx.lineWidth = "5"
 ctx.beginPath();
 ctx.moveTo(85, 300);
 ctx.lineTo(185, 300);
 ctx.lineTo(185, 400);
 ctx.lineTo(85, 400);
 ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+
+//circle
+ctx.beginPath();
+ctx.arc(385,440,50,0, (3*Math.PI)/2, false);
+ctx.lineTo(385,440);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
